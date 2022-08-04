cc903888c12364600b8c2e3fb34fb4455f36164a
lawen <lawensardar16@gmail.com>
2022-08-04 12:05:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", ()=> {
+
+  const p =  document.getElementById('text')
+    p.textContent = ('This is really cool!')
+});
+
+  
